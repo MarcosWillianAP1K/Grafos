08f8e3ad9477d3aef31aa8734c901b91cff5a123
MarcosWillianAP1K <mwss@outlook.com>
2025-06-24 16:00:04 -0300

    Inserir vertices, so o remover que ta zuado

--- teste_social.c
+++ teste_social.c
@@ -13,7 +13,7 @@ typedef struct VERTICE
 {
     int id;   // Identificador do vértice
     int grau; // Grau do vértice
-    int peso; // Peso do vértice, se for ponderado
+    int peso; // Peso do vértice, s e for ponderado
 } VERTICE;
 
 typedef struct VETOR_ADJACENCIA
@@ -26,7 +26,7 @@ typedef struct GRAFO
 {
     short int eh_ponderado;             // 1 se o grafo é ponderado, 0 se não
     short int eh_digrafo;               // 1 se o grafo é um dígrafo, 0 se não
-    int grau_max;                       // Grau máximo do grafo
+    int n_vertices;                     // Número de vértices do grafo
     VETOR_ADJACENCIA *vetor_adjacencia; // Vetor de listas de adjacência
 } GRAFO;
 
@@ -52,38 +52,26 @@ ARESTA *criar_aresta(int vertice_destino, int peso)
 }
 
 // Função para criar um grafo com grau máximo, ponderado e direcionado
-GRAFO *criar_grafo(int grau_max, short int eh_ponderado, short int eh_digrafo)
+GRAFO iniciar_grafo(short int eh_ponderado, short int eh_digrafo)
 {
-    GRAFO *grafo = (GRAFO *)malloc(sizeof(GRAFO));
+    GRAFO grafo;
 
-    verificar_alocacao(grafo, "falha ao alocar memória para o grafo");
-
-    grafo->eh_ponderado = eh_ponderado;
-    grafo->eh_digrafo = eh_digrafo;
-    grafo->grau_max = grau_max;
-    grafo->vetor_adjacencia = (VETOR_ADJACENCIA *)malloc((grau_max + 1) * sizeof(VETOR_ADJACENCIA));
-
-    verificar_alocacao(grafo->vetor_adjacencia, "falha ao alocar memória para o vetor de adjacência");
-
-    for (int i = 0; i < grau_max + 1; i++)
-    {
-        grafo->vetor_adjacencia[i].vertice.id = i + 1;
-        grafo->vetor_adjacencia[i].vertice.grau = 0;
-        grafo->vetor_adjacencia[i].vertice.peso = 0;
-        grafo->vetor_adjacencia[i].lista_aresta = NULL;
-    }
+    grafo.eh_ponderado = eh_ponderado;
+    grafo.eh_digrafo = eh_digrafo;
+    grafo.n_vertices = 0;
+    grafo.vetor_adjacencia = NULL;
 
     return grafo;
 }
 
 // Função para liberar a memória alocada para o grafo
-void liberar_grafo(GRAFO **grafo)
+void liberar_grafo(GRAFO *grafo)
 {
-    if (grafo != NULL && *grafo != NULL)
+    if (grafo != NULL)
     {
-        for (int i = 0; i < (*grafo)->grau_max; i++)
+        for (int i = 0; i < grafo->n_vertices; i++)
         {
-            ARESTA *aresta_atual = (*grafo)->vetor_adjacencia[i].lista_aresta;
+            ARESTA *aresta_atual = grafo->vetor_adjacencia[i].lista_aresta;
             while (aresta_atual)
             {
                 ARESTA *temp = aresta_atual;
@@ -91,18 +79,20 @@ void liberar_grafo(GRAFO **grafo)
                 free(temp);
             }
         }
-        free((*grafo)->vetor_adjacencia);
-        free(*grafo);
-        *grafo = NULL;
+        free(grafo->vetor_adjacencia);
+        grafo->vetor_adjacencia = NULL;
+        grafo->n_vertices = 0;
     }
 }
+
+//=========================ARESTAS==========================
 
 short int inserir_aresta(GRAFO *grafo, int vertice_origem, int vertice_destino, int peso)
 {
     short int retorno = 0;
 
     // Verificações necessarias para inserir a aresta
-    if (grafo != NULL && vertice_origem > 0 && vertice_origem <= grafo->grau_max && vertice_destino > 0 && vertice_destino <= grafo->grau_max && ((!grafo->eh_ponderado && peso == 1) || (grafo->eh_ponderado)))
+    if (grafo != NULL && vertice_origem > 0 && vertice_origem <= grafo->n_vertices && vertice_destino > 0 && vertice_destino <= grafo->n_vertices && ((!grafo->eh_ponderado && peso == 1) || (grafo->eh_ponderado)))
     {
         ARESTA *nova_aresta = criar_aresta(vertice_destino, peso);
 
@@ -129,7 +119,7 @@ short int remover_aresta(GRAFO *grafo, int vertice_origem, int vertice_destino)
 {
     short int retorno = 0;
 
-    if (grafo != NULL && vertice_origem > 0 && vertice_origem <= grafo->grau_max && vertice_destino > 0 && vertice_destino <= grafo->grau_max)
+    if (grafo != NULL && grafo->n_vertices > 0 && vertice_origem > 0 && vertice_origem <= grafo->n_vertices && vertice_destino > 0 && vertice_destino <= grafo->n_vertices)
     {
         ARESTA *atual = grafo->vetor_adjacencia[vertice_origem - 1].lista_aresta;
         ARESTA *anterior = NULL;
@@ -167,13 +157,109 @@ short int remover_aresta(GRAFO *grafo, int vertice_origem, int vertice_destino)
     return retorno;
 }
 
-void imprimir_matriz_grafo(GRAFO *grafo)
+//=========================VERTICES=========================
+
+short int inserir_vertice(GRAFO *grafo, int peso)
+{
+    short int retorno = 0;
+
+    if (grafo != NULL)
+    {
+        // Realoca o vetor de adjacência para incluir o novo vértice
+        VETOR_ADJACENCIA *novo_vetor = realloc(grafo->vetor_adjacencia, (grafo->n_vertices + 1) * sizeof(VETOR_ADJACENCIA));
+
+        verificar_alocacao(novo_vetor, "falha ao alocar memória para o vetor de adjacência");
+
+        grafo->vetor_adjacencia = novo_vetor;
+        grafo->vetor_adjacencia[grafo->n_vertices].vertice.id = grafo->n_vertices + 1;
+        grafo->vetor_adjacencia[grafo->n_vertices].vertice.peso = peso;
+        grafo->vetor_adjacencia[grafo->n_vertices].vertice.grau = 0;
+        grafo->vetor_adjacencia[grafo->n_vertices].lista_aresta = NULL;
+
+        grafo->n_vertices++;
+        retorno = 1; // Indica que o vértice foi inserido com sucesso
+    }
+
+    return retorno;
+}
+
+
+//Remover ainda possui falhas, ja que o id dos vertices não é atualizado corretamente nas listas de arestas
+short int remover_vertice(GRAFO *grafo, int id_vertice)
+{
+    short int retorno = 0;
+
+    if (grafo != NULL && id_vertice > 0 && id_vertice <= grafo->n_vertices)
+    {
+        // Remove todas as arestas que apontam para o vértice a ser removido
+        if (grafo->eh_digrafo)
+        {
+            // Se é um dígrafo, remove todas as arestas que apontam para o vértice de destino
+            //Tem q passar por todos os vértices e remover as arestas que apontam para o vértice de destino
+            for (int i = 0; i < grafo->n_vertices; i++)
+            {
+                if (i != id_vertice - 1) // Não remover arestas do próprio vértice
+                {
+                    remover_aresta(grafo, i + 1, id_vertice);
+                }
+            }
+        }
+        else
+        {
+            //Se não é um dígrafo, remove todas as arestas do vértice de origem
+            // Percorre a lista de arestas do vértice de origem
+            ARESTA *atual = grafo->vetor_adjacencia[id_vertice - 1].lista_aresta;
+
+            while (atual != NULL)
+            {
+                ARESTA *temp = atual;
+                atual = atual->proximo;
+
+                // Remove a aresta do vértice de origem
+                remover_aresta(grafo, temp->vertice_destino, id_vertice);
+            }
+        }
+
+        
+        // Libera a memória alocada para as arestas do vértice
+        ARESTA *aresta_atual = grafo->vetor_adjacencia[id_vertice - 1].lista_aresta;
+        while (aresta_atual != NULL)
+        {
+            ARESTA *temp = aresta_atual;
+            aresta_atual = aresta_atual->proximo;
+            free(temp);
+        }
+
+        // Realoca o vetor de adjacência para remover o vértice
+        for (int i = id_vertice - 1; i < grafo->n_vertices - 1; i++)
+        {
+            grafo->vetor_adjacencia[i + 1].vertice.id--;
+            grafo->vetor_adjacencia[i] = grafo->vetor_adjacencia[i + 1];
+        }
+
+        grafo->vetor_adjacencia[grafo->n_vertices - 1].lista_aresta = NULL; // Limpa a lista de arestas do último vértice
+
+        VETOR_ADJACENCIA *novo_vetor = realloc(grafo->vetor_adjacencia, (grafo->n_vertices - 1) * sizeof(VETOR_ADJACENCIA));
+        verificar_alocacao(novo_vetor, "falha ao alocar memória para o vetor de adjacência");
+
+        grafo->vetor_adjacencia = novo_vetor;
+        grafo->n_vertices--;
+
+        retorno = 1; // Indica que o vértice foi removido com sucesso
+    }
+
+    return retorno;
+}
+
+//=========================IMPRIMIR=========================
+
+void imprimir_lista_grafo(GRAFO *grafo)
 {
     if (grafo != NULL)
     {
 
-        printf("\nMatriz de Adjacencia do Grafo:\n");
-        for (int i = 0; i < grafo->grau_max + 1; i++)
+        printf("\nLista de Adjacencia do Grafo:\n");
+        for (int i = 0; i < grafo->n_vertices; i++)
         {
             printf("Vertice %d: ", grafo->vetor_adjacencia[i].vertice.id);
             ARESTA *aresta_atual = grafo->vetor_adjacencia[i].lista_aresta;
@@ -192,34 +278,53 @@ void imprimir_matriz_grafo(GRAFO *grafo)
 
 int main()
 {
-    int grau_max = 5;
     short int eh_ponderado = 0; // Grafo ponderado
     short int eh_digrafo = 0;   // Grafo não direcionado
 
-    GRAFO *grafo = criar_grafo(grau_max, eh_ponderado, eh_digrafo);
+    GRAFO grafo = iniciar_grafo(eh_ponderado, eh_digrafo);
 
     printf("Grafo criado com sucesso!\n");
-    printf("Grau maximo: %d\n", grafo->grau_max);
-    printf("Eh ponderado: %d\n", grafo->eh_ponderado);
-    printf("Eh digrafo: %d\n", grafo->eh_digrafo);
+    printf("Numero de vertices: %d\n", grafo.n_vertices);
+    printf("Eh ponderado: %d\n", grafo.eh_ponderado);
+    printf("Eh digrafo: %d\n", grafo.eh_digrafo);
 
-    // Inserindo arestas no grafo
-    inserir_aresta(grafo, 1, 2, 1);
-    inserir_aresta(grafo, 1, 3, 1);
-    inserir_aresta(grafo, 2, 4, 1);
+
+    // Inserindo vértices no grafo
+    inserir_vertice(&grafo, 1); // Vértice 1  
+    inserir_vertice(&grafo, 1); // Vértice 2
+    inserir_vertice(&grafo, 1); // Vértice 3
+    inserir_vertice(&grafo, 1); // Vértice 4
+    inserir_vertice(&grafo, 1); // Vértice 5
+
 
     printf("\n");
-    // Imprimindo a matriz de adjacência do grafo
-    imprimir_matriz_grafo(grafo);
+    // Imprimindo a lista de adjacência do grafo após a inserção dos
+    // vértices
+    imprimir_lista_grafo(&grafo);
+
+    // Inserindo arestas no grafo
+    inserir_aresta(&grafo, 1, 2, 1);
+    inserir_aresta(&grafo, 1, 3, 1);
+    inserir_aresta(&grafo, 2, 4, 1);
+
+    printf("\n");
+    // Imprimindo a lista de adjacência do grafo
+    imprimir_lista_grafo(&grafo);
 
     // Removendo uma aresta do grafo
-    remover_aresta(grafo, 1, 2);
+    remover_aresta(&grafo, 1, 2);
 
     printf("\nAresta (1, 2) removida.\n");
 
-    // Imprimindo a matriz de adjacência do grafo após a remoção
+    // Imprimindo a lista de adjacência do grafo após a remoção
     printf("\n");
-    imprimir_matriz_grafo(grafo);
+    imprimir_lista_grafo(&grafo);
+
+    
+
+    // Imprimindo a lista de adjacência do grafo após a remoção do vértice
+    printf("\n");
+    imprimir_lista_grafo(&grafo);
 
     // Libera a memória alocada para o grafo
     liberar_grafo(&grafo);
